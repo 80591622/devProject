450f0965a27ecd36ac0b9061259b854c36e0867c
wangke <python.wangke@gmail.com>
2025-02-26 17:49:31 +0800

    实现tabs路由切换

--- react-bill-test/src/page/Layout/index.js
+++ react-bill-test/src/page/Layout/index.js
@@ -1,30 +1,57 @@
-import { Outlet } from "react-router-dom"
-import { Button } from "antd-mobile"
+import { TabBar } from "antd-mobile"
 import { useEffect } from "react"
-import { useDispatch } from "react-redux"
+import { Outlet, useNavigate } from "react-router-dom"
+import { useDispatch } from 'react-redux'
 import { getBillList } from "@/store/modules/billStore"
+import './index.scss'
+import {
+  BillOutline,
+  CalculatorOutline,
+  AddCircleOutline
+} from 'antd-mobile-icons'
 
-
+const tabs = [
+  {
+    key: '/month',
+    title: '月度账单',
+    icon: <BillOutline />,
+  },
+  {
+    key: '/new',
+    title: '记账',
+    icon: <AddCircleOutline />,
+  },
+  {
+    key: '/year',
+    title: '年度账单',
+    icon: <CalculatorOutline />,
+  },
+]
 
 const Layout = () => {
   const dispatch = useDispatch()
-
   useEffect(() => {
     dispatch(getBillList())
-  },[dispatch])
+  }, [dispatch])
 
-
+  // 切换菜单跳转路由
+  const navigate = useNavigate()
+  const switchRoute = (path) => {
+    console.log(path)
+    navigate(path)
+  }
   return (
-    <div>
-      <Outlet></Outlet>
-      Layout 页面
-      <Button color="primary">全局</Button>
-
-      <div className="puple">
-        <Button color="primary">局部样式</Button>
+    <div className="layout">
+      <div className="container">
+        <Outlet />
       </div>
-      
-
+      <div className="footer">
+        <TabBar onChange={switchRoute}>
+          {tabs.map(item => (
+            <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+          ))}
+        </TabBar>
+      </div>
     </div>
   )
 }
